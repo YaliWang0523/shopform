7be93c50c26d43fd49a162ef553278ce5e840554
yali-mac <yali.wang.0523@gmail.com>
2018-12-29 15:34:20 +0800

    壞掉的seller nav

--- src/components/seller/seller_list.vue
+++ src/components/seller/seller_list.vue
@@ -1,12 +1,12 @@
 <template >
   <div>
   <!-- Sidenav 縮小會壞掉-->
-    <SellerNav/>
+  <SellerNav/>
+  <div class="main-content">
     <!-- Header -->
     <div class="header bg-gradient-primary pb-8 pt-5 pt-md-8">
       <div class="container-fluid">
         <div class="header-body">
-
         </div>
       </div>
     </div>
