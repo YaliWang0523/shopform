313c476b3026046df00b31aad8a364a7104c940f
yali-mac <yali.wang.0523@gmail.com>
2019-01-10 17:19:53 +0800

    改js路徑 拿掉賣家微微案鄭

--- src/components/seller/seller_close_order.vue
+++ src/components/seller/seller_close_order.vue
@@ -11,14 +11,6 @@
         <ul class="navbar-nav align-items-center d-none d-md-flex">
           <li class="nav-item dropdown">
             <a class="nav-link pr-0" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-              <div class="media align-items-center">
-                <span class="avatar avatar-sm rounded-circle">
-                  <img alt="Image placeholder" src="static/assets/img/theme/team-4-800x800.jpg">
-                </span>
-                <div class="media-body ml-2 d-none d-lg-block">
-                  <span class="mb-0 text-sm  font-weight-bold">薇薇安鄭</span>
-                </div>
-              </div>
             </a>
             <div class="dropdown-menu dropdown-menu-arrow dropdown-menu-right">
               <div class=" dropdown-header noti-title">
